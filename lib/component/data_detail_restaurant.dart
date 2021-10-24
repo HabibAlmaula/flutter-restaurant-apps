9b2458cd253f2f7d9e0f34a00b3d54c930a06917
Habib Almaula <habibalmaula@gmail.com>
2021-10-25 05:09:47 +0700

    split widget detail restaurant

---
+++ lib/component/data_detail_restaurant.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restaurant_app/component/circular_modal.dart';
+import 'package:restaurant_app/component/content_modal.dart';
+import 'package:restaurant_app/component/restauran_item.dart';
+import 'package:restaurant_app/data/models/detailrestaurant.dart';
+import 'package:restaurant_app/data/service/api_service.dart';
+
+class DataDetail extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+  static const double kExpandedHeight = 200;
+  final DetailRestaurant detailRestaurant;
+
+  DataDetail({Key? key, required this.detailRestaurant}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      controller: _scrollController,
+      headerSliverBuilder: (context, isScroleld) {
+        return [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: kExpandedHeight,
+            flexibleSpace: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              var top = constraints.biggest.height;
+
+              return FlexibleSpaceBar(
+                background: Hero(
+                  tag: detailRestaurant.restaurant.pictureId,
+                  child: Image.network(
+                    ApiService.baseUrlPictureLarge +
+                        detailRestaurant.restaurant.pictureId,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                title: Text(
+                  top > 80 && top < 125 ? detailRestaurant.restaurant.name : "",
+                ),
+                titlePadding: EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: _horizontalTitlePadding + 16.0),
+              );
+            }),
+          )
+        ];
+      },
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          Column(
+            children: [
+              Card(
+                elevation: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        detailRestaurant.restaurant.name,
+                        style: GoogleFonts.fjallaOne(
+                          textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.location_on,
+                              size: 20.0,
+                              color: Colors.red,
+                            ),
+                            const SizedBox(
+                              width: 1,
+                            ),
+                            Text(
+                              detailRestaurant.restaurant.city,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.w300),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        detailRestaurant.restaurant.address,
+                        style: const TextStyle(fontSize: 13.0),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Wrap(
+                            spacing: 6.0,
+                            runSpacing: 6.0,
+                            children: [
+                              for (var i
+                                  in detailRestaurant.restaurant.categories)
+                                buildChip(i.name)
+                            ],
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                        child: const Text(
+                          "Description",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
+                        child: Text(
+                          detailRestaurant.restaurant.description,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  "Menu",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      margin: const EdgeInsets.only(
+                          right: 16.0, left: 16.0, top: 20.0),
+                      child: InkWell(
+                        onTap: () {
+                          showAvatarModalBottomSheet(
+                              title: "food",
+                              context: context,
+                              builder: (context) {
+                                List<String> listContent = [];
+                                for (var element in detailRestaurant
+                                    .restaurant.menus.foods) {
+                                  listContent.add(element.name);
+                                }
+                                return ModalContent(
+                                    title: "Food", content: listContent);
+                              });
+                        },
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+                              child: Image.asset(
+                                "assets/images/food.png",
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                              child: Text(
+                                "Foods",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      margin: const EdgeInsets.only(
+                          right: 16.0, left: 16.0, top: 20.0),
+                      child: InkWell(
+                        onTap: () {
+                          showAvatarModalBottomSheet(
+                              title: "drink",
+                              context: context,
+                              builder: (context) {
+                                List<String> listContent = [];
+                                for (var element in detailRestaurant
+                                    .restaurant.menus.drinks) {
+                                  listContent.add(element.name);
+                                }
+                                return ModalContent(
+                                    title: "Drink", content: listContent);
+                              });
+                        },
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+                              child: Image.asset(
+                                "assets/images/drink.png",
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                              child: Text(
+                                "Drinks",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  double get _horizontalTitlePadding {
+    const kBasePadding = 15.0;
+    const kMultiplier = 0.5;
+
+    if (_scrollController.hasClients) {
+      if (_scrollController.offset < (kExpandedHeight / 2)) {
+        return kBasePadding;
+      }
+
+      if (_scrollController.offset > (kExpandedHeight - kToolbarHeight)) {
+        return (kExpandedHeight / 2 - kToolbarHeight) * kMultiplier +
+            kBasePadding;
+      }
+
+      return (_scrollController.offset - (kExpandedHeight / 2)) * kMultiplier +
+          kBasePadding;
+    }
+
+    return kBasePadding;
+  }
+}
